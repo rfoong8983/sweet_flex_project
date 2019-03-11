9d9652fd0f944a3dd02ca9a8aa44638e7285d91d
danny yan <danny.yan@gmail.com>
2019-03-10 18:23:13 -0700

    Create Login and Register

--- app/app.js
+++ app/app.js
@@ -7,13 +7,18 @@ const db = require('./config/keys').mongoURI;
 
 const bodyParser = require('body-parser')
 const passport = require('passport')
-
+const users = require('./routes/api/users')
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 const port = process.env.PORT || 5000; // run on 5000
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());   
+app.use("/api/users", users);
+
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
