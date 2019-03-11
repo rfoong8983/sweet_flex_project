9494c529c3dfdf55e58ad4d6a23d3cf8d85e7afc
danny yan <danny.yan@gmail.com>
2019-03-11 13:40:53 -0700

    add folder

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './components/navbar_container';
 import NavBar from './components/navbar';
-import ProfileContainer from './components/profile_container'
+import ProfileContainer from './components/profile/profile_container'
 import {Authroute, Protectedroute} from './util/route_util';
 
 const App = () => (
