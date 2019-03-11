c41f775b55d363ee3facf29ed0f86b3c34043318
danny yan <danny.yan@gmail.com>
2019-03-11 00:58:21 -0700

    Add Passport to Users

--- app/app.js
+++ app/app.js
@@ -15,9 +15,14 @@ mongoose
     .catch(err => console.log(err));
 const port = process.env.PORT || 5000; // run on 5000
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());   
 app.use("/api/users", users);
+
 
 
 
--- app/routes/api/users.js
+++ app/routes/api/users.js
@@ -13,6 +13,14 @@ const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        username: req.user.username,
+    });
+})
+  
+
 router.post('/register', (req, res) => {
 
     // const {errors, isValid} = validateRegisterInput(req.body);
