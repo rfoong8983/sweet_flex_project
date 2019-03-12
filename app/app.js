2a957b5ee0be349fbbb78a472c15a0c6eb9cc8f6
ryan <ryanfoong8983@gmail.com>
2019-03-12 16:23:31 -0700

    change db in app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = process.env.MONGO_URI;
 
 const bodyParser = require('body-parser')
 const passport = require('passport')
