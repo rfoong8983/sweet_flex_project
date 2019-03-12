2a4e8a9d579f39b13187d033833170f640788b8d
ryan <ryanfoong8983@gmail.com>
2019-03-12 11:30:46 -0700

    Setup production keys on heroku. Setup heroku scripts in package.json

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -8,16 +9,19 @@ const passport = require('passport')
 const users = require('./routes/api/users')
 const searches = require('./routes/api/search')
 
-// for development
-// mongoose
-//     .connect(db, { useNewUrlParser: true })
-//     .then(() => console.log("Connected to MongoDB successfully"))
-//     .catch(err => console.log(err));
-// const port = process.env.PORT || 5000; // run on 5000
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 const port = process.env.PORT || 5000; // run on 5000
+
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
