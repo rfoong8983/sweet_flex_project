20c44c3a5774f68d8bf12b6d28646be8bce77535
your name here <gradyzhoo@gmail.com>
2019-03-11 22:50:20 -0700

    Create Dashboard components, create new frontend route

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './components/navbar/navbar_container';
+import DashboardContainer from './components/search/dashboard_container';
 
 const App = () => (
     <div className="appWrapper">
         <NavBarContainer />
+        <Route exact path="/dashboard" component={DashboardContainer}/>
     </div>
 )
 
