acabeef3dbf15d1eb38119b97d4a53fbcbca0b3f
ryan <ryanfoong8983@gmail.com>
2019-04-02 13:23:28 -0700

    Add watson key to prod

--- app/config/keys_prod.js
+++ app/config/keys_prod.js
@@ -1,6 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
+    watsonAPIKey: process.env.WATSON_API_KEY,
     twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
     twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
     twitterAccessToken: process.env.TWITTER_ACCESS_TOKEN,
