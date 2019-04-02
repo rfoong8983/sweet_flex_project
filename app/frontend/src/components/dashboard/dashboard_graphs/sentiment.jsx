c58ead9bc19b5ee61c7289db0f3f0452f25afc1f
ryan <ryanfoong8983@gmail.com>
2019-04-02 16:59:17 -0700

    Calculate summary statistics for score and comparative score for tweets pulled using Twitter API

--- app/frontend/src/components/dashboard/dashboard_graphs/sentiment.jsx
+++ app/frontend/src/components/dashboard/dashboard_graphs/sentiment.jsx
@@ -17,19 +17,61 @@ class SentGraph extends Component { // bar graph
         });
     }
 
-    aggregate() {
+    averages() {
         const averages = {};
-        const comp = 0;
-        const score = 0;
+        let comparative = null;
+        let score = null;
+        let maxComparative = null;
+        let maxScore = null;
+        let minComparative = null;
+        let minScore = null;
+
+        const len = this.state.graphData.length;
         this.state.graphData.forEach((el) => {
-            
+            if (score === null && comparative === null) {
+                score = el.score;
+                comparative = el.comparative;
+                maxScore = el.score;
+                minScore = el.score;
+                maxComparative = el.comparative;
+                minComparative = el.comparative;
+            } else {
+                score += el.score
+                comparative += el.comparative;
+            }
+
+            if (el.score > maxScore) {
+                maxScore = el.score;
+            } else if (el.comparative > maxComparative) {
+                maxComparative = el.comparative;
+            }
+
+            if (el.score < minScore) {
+                minScore = el.score;
+            } else if (el.comparative < minComparative) {
+                minComparative = el.comparative;
+            }
         })
+
+        return {
+            score: Math.floor(score/len),
+            comparative: comparative/len,
+            maxScore,
+            maxComparative,
+            minScore,
+            minComparative
+        }
     }
 
     render() {
         return (
             <div className="sentGraph">
-                {this.test}
+                <li>Score: {this.averages().score}</li>
+                <li>Comparative: {this.averages().comparative}</li>
+                <li>Max Score: {this.averages().maxScore}</li>
+                <li>Max Comparative: {this.averages().maxComparative}</li>
+                <li>Min Score: {this.averages().minScore}</li>
+                <li>Min Comparative: {this.averages().minComparative}</li>
             </div>
         )
     }
